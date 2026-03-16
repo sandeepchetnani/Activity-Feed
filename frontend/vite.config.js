14c7fb32b798c57aefc1cc4bb63adf12a095817f
sandeep Chetnani <sandeep.lekhraj@medibuddy.in>
2026-03-16 22:37:28 +0530

    changed backend base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5004',
+        // target: 'http://localhost:5004',
+        target: 'https://activity-feed-dusky.vercel.app',
         changeOrigin: true
       }
     }
